24a43eaa0e6e63bd21ca84155f882f2862167f21
Bilge Kızgındere <bilgekizgindere@gmail.com>
2025-06-01 14:05:22 +0300

    initialize commit

---
+++ src/main/java/com/bilgekdere/RabbitMqWithSpringDemo/RabbitMqWithSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bilgekdere.RabbitMqWithSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RabbitMqWithSpringDemoApplication {
+
+	public static void main(String[] args)  {
+		SpringApplication.run(RabbitMqWithSpringDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bilgekdere/RabbitMqWithSpringDemo/RabbitMqWithSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgekdere.RabbitMqWithSpringDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqWithSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
